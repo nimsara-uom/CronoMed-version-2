17bf1def8124ba923a3e950ab39f45d11dc73dd7
risandu26-hash <risandu26@gmail.com>
2026-07-05 21:04:04 +0530

    Update client.js

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,5 +1,8 @@
 import axios from 'axios';
+//import axios
 
+
+// api var
 const api = axios.create({
   baseURL: 'http://localhost:8080/api',
 });
@@ -28,3 +31,5 @@ api.interceptors.response.use(
 );
 
 export default api;
+
+
